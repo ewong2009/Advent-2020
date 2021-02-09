4428140216d74e9c759beee6e6087dde05aa1a28
Ervin Wong <ewong2009@gmail.com>
2021-02-09 16:54:33 -0500

    Delete Day 8 directory

--- Day 8/instruction.py
+++
@@ -1,9 +0,0 @@
-# Object to hold all three values of an instruction
-class Instruction():
-    def __init__(self,action,value):
-        self.action = action
-        self.value = value
-        self.seen = False
-    
-    def display(self):
-        print(self.action, self.value, self.seen)
